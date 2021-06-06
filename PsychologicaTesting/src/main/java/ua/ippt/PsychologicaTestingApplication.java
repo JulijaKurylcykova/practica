8fd5714f3de65a440ad70a2b6c2d797b39f785ac
Julia <juliakurylo18@gmail.com>
2021-06-06 22:16:07 +0300

    initial commit

---
+++ PsychologicaTesting/src/main/java/ua/ippt/PsychologicaTestingApplication.java
@@ -0,0 +1,13 @@
+package ua.ippt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicaTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologicaTestingApplication.class, args);
+	}
+
+}
---
+++ PsychologicaTesting/src/test/java/ua/ippt/PsychologicaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ippt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicaTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
